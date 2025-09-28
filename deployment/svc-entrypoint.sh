c2f29765b6e2452c231b52d1a043de045fcfcd8e
brad.sun <brad.sun@inferx.net>
2025-09-28 13:11:21 -0700

    add coredump support svc

--- deployment/svc-entrypoint.sh
+++ deployment/svc-entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -e
 
+if [ "${ENABLE_COREDUMP}" = "true" ]; then
+  echo "Enable coredump"
+  # allow unlimited core file size
+  ulimit -c unlimited
+  # set core dump file location & naming
+  echo "/opt/inferx/log/core.na.%e.%p" > /proc/sys/kernel/core_pattern
+else
+  echo "Disable coredump"
+  # disallow core dumps
+  ulimit -c 0
+fi
+
 ./svc "$@"
