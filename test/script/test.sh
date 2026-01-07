bb247afca04f88a0d35043faff7d02d847153c44
Fib <fib161818@gmail.com>
2026-01-07 00:02:05 +0000

    deployment artifacts update

--- test/script/test.sh
+++ test/script/test.sh
@@ -3,6 +3,9 @@ nohup ./stopstatesvc.sh > stopstatesvc.log 2>&1 &
 nohup test/script/stress.sh > stress.log 2>&1 &
 nohup test/script/stopscheduler.sh > stopscheduler.log 2>&1 &
 
+nohup test/script/stopstatesvc.sh > stopstatesvc.log 2>&1 &
+
+
 ps aux | grep stop
 
 ps aux | grep stress
